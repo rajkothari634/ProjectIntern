07bc0c98b3dc334a3acfeae2cf217e43ceee1d83
Raj kothari <rkmeansrk39@gmail.com>
2020-08-27 10:55:46 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,19 +19,14 @@ const Field = (props) => {
 function createData(word, freq) {
   return { word, freq };
 }
-var datachange = [];
-var loading = false;
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [fetched, setFetched] = useState(false);
   const [fetchedData, setFetchedData] = useState([]);
-  const [queryNum, setQueryNum] = useState(0);
-  const fetchData = () => {
+  const fetchData = (queryNum) => {
     if (loading) {
       return;
     }
-    setFetched(false);
     setLoading(true);
     fetch("http://localhost:5050/getfrequentword?q=" + queryNum)
       .then((res) => res.json())
@@ -43,34 +38,35 @@ function App() {
         for (let i = 0; i < words.length; i++) {
           rows.push(createData(words[i], freq[i]));
         }
-        setFetched(true);
         setLoading(false);
         setFetchedData(rows);
       });
   };
-  const handlequery = (event) => {
-    console.log(event.target.value);
-    setQueryNum(event.target.value);
-  };
+  // const handlequery = (event) => {
+  //   console.log(event.target.value);
+  //   setQueryNum(event.target.value);
+  // };
 
   return (
     <div style={{ backgroundColor: "#cacaca", height: "800px" }}>
       <Grid containerjustify="center">
         <Grid item md={6} xs={12}>
-          <Field
-            id={"querynumber"}
-            label={"Enter number"}
-            onChange={handlequery}
-          />
+          <Field id={"querynumber"} label={"Enter number"} />
         </Grid>
 
         <Grid item md={6} xs={12} style={{ marginTop: "30px" }}>
-          <Button variant="contained" color="primary" onClick={fetchData(10)}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              fetchData(10);
+            }}
+          >
             Fetch Max Freq Words
           </Button>
         </Grid>
       </Grid>
-      {fetchData.length > 0 ? (
+      {!loading ? (
         <Grid item md={6} xs={12} style={{ marginTop: "30px" }}>
           <StyledTable rows={fetchedData} />
         </Grid>
